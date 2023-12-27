66d3c8d4b949fd86f64a298575ae640e6548a1a2
samsul kobir <makkobir79@gmail.com>
2023-12-28 00:17:03 +0200

    footer created

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../pages/shared/Header";
 import { Container } from "@chakra-ui/react";
+import Footer from "../pages/shared/Footer";
 
 function MainLayout() {
   return (
@@ -9,6 +10,7 @@ function MainLayout() {
       <Container maxW="container.xl">
         <Outlet />
       </Container>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/shared/Footer.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Flex bg="gray.900" align="center" justify="center">
+      <p style={{ color: "white", padding: "20px" }}>
+        copyright All right reserved @ 2020
+      </p>
+    </Flex>
+  );
+};
+
+export default Footer;
