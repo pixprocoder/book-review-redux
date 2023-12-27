f74ca19a497f0f4670540514f82ae72436271973
samsul kobir <makkobir79@gmail.com>
2023-12-28 01:07:04 +0200

    added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./features/counterSlice";
+import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice,
+    auth: authReducer,
   },
 });
 
