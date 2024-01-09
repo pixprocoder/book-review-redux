a944a409d84bcbb77f71d80c7f0af7018bd4656e
samsul kobir <makkobir79@gmail.com>
2024-01-09 23:59:17 +0200

    edit book modified

--- src/pages/books/EditBook.tsx
+++ src/pages/books/EditBook.tsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useSingleBookQuery } from "../../redux/api/apiSlice";
 
 const EditBook = () => {
+  // const { data, isLoading, isSuccess } = useSingleBookQuery(2);
   return (
     <Box>
       <h1 style={{ textAlign: "center" }}>This is edit Book page</h1>
+
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        aliquam voluptates tenetur veritatis, molestiae delectus rem non quis id
+        minus quidem tempora? Repudiandae unde quisquam ducimus officiis error
+        natus. Tempora?
+      </h1>
+      <Button>Update</Button>
     </Box>
   );
 };
