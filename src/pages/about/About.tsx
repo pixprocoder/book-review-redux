c8628f53bf34494b8035bf39a3078f34c2c0c7df
samsul kobir <makkobir79@gmail.com>
2024-01-09 23:52:40 +0200

    about section edited

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,16 @@
+import { Box, Text } from "@chakra-ui/react";
+
 const About = () => {
-  return <div>This is about page</div>;
+  return (
+    <Box>
+      <h1>About Us</h1>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus ut,
+        magnam quaerat facilis recusandae ex eum architecto animi voluptatem
+        corrupti. Minus voluptates velit eos aliquid sed sunt eaque libero ex?
+      </Text>
+    </Box>
+  );
 };
 
 export default About;
